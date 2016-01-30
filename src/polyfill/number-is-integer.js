97103d5a7252d58e743ad4058c1d251031612982
zach lysobey <zach@lysobey.com>
2016-01-30 12:05:29 -0500

    add attribution to polyfill

--- src/polyfill/number-is-integer.js
+++ src/polyfill/number-is-integer.js
@@ -1,3 +1,7 @@
+/**
+ * Attribution: @link http://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger#Polyfill
+ * Browser support table: @link http://kangax.github.io/compat-table/es6/#Number.isInteger
+ */
 Number.isInteger = Number.isInteger || function(value) {
   return typeof value === "number" &&
     isFinite(value) &&
