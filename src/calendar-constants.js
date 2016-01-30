56930945fbc4759bb426e563d5258acb5f15b28e
zach lysobey <zach@lysobey.com>
2016-01-30 11:10:40 -0500

    extract calendar-constants module

---
+++ src/calendar-constants.js
@@ -0,0 +1,14 @@
+export const MONTHS = Object.freeze([
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]);
--- src/calendar-controller.js
+++ src/calendar-controller.js
@@ -1,19 +1,4 @@
-'use strict';
-
-const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-];
+import {MONTHS as months} from './calendar-constants';
 
 export function getMonth(monthIndex) {
     return new MonthModel(monthIndex);
