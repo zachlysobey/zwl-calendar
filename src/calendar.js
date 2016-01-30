c8683d9bffe4f6dc4aa728b0f2bd5512351b2f25
zach lysobey <zach@lysobey.com>
2016-01-30 13:34:11 -0500

    start rendering calendar controls to the DOM

--- src/calendar.js
+++ src/calendar.js
@@ -3,6 +3,42 @@ import CalendarController from './calendar-controller';
 export function init(id) {
     const calendarCtrl = new CalendarController();
     const now = new Date();
+
     const calendarElement = document.getElementById(id);
-    calendarElement.innerHTML = calendarCtrl.getMonth(now.getMonth);
+
+    const currentMonth = calendarCtrl.getMonth(now.getMonth());
+
+    calendarElement.innerHTML = buildCalendar(currentMonth);
+
+    const previousMonthLink = calendarElement.getElementsByClassName('prev-month')[0];
+    const nextMonthLink = calendarElement.getElementsByClassName('next-month')[0];
+    const currentMonthTitle = calendarElement.getElementsByClassName('current-month')[0];
+
+    previousMonthLink.addEventListener('click', () => {
+        const prevMonth = calendarCtrl.previousMonth();
+        currentMonthTitle.innerHTML = prevMonth.month;
+        return false;
+    });
+
+    nextMonthLink.addEventListener('click', () => {
+        const nextMonth = calendarCtrl.nextMonth();
+        currentMonthTitle.innerHTML = nextMonth.month;
+        return false;
+    });
+}
+
+function buildCalendar(month) {
+    return `
+        <zwl-calendar>
+            <section class="month-view">
+                <header class="month-navigation">
+                    <a href="#" class="prev-month">&lt;</a>
+                    <span class="current-month">
+                        ${month.month}
+                    </span>
+                    <a href="#" class="next-month">&gt;</a>
+                </header>
+            <section>
+        </zwl-calendar>
+    `;
 }
--- test/calendar.spec.js
+++ test/calendar.spec.js
@@ -6,4 +6,20 @@ describe('calendar widget', () => {
         expect(typeof zwlCalendar.init).toEqual('function');
     });
 
+    it('should insert a calendar object into the specified id', () => {
+        const id = 'some-id';
+        const el = addElementToDOMWithId(id);
+
+        zwlCalendar.init(id);
+
+        expect(el.getElementsByTagName('zwl-calendar')[0]).toBeTruthy();
+    });
+
+    function addElementToDOMWithId(id) {
+        const el = document.createElement('div');
+        el.id = id;
+        document.body.appendChild(el);
+        return el;
+    }
+
 });
