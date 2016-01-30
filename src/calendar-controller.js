e4e61f10e07d99a3ce8399a6d826a6226513c4f3
zach lysobey <zach@lysobey.com>
2016-01-30 13:44:34 -0500

    refactor calendar-controler

--- src/calendar-controller.js
+++ src/calendar-controller.js
@@ -5,33 +5,37 @@ export default function CalendarController() {
 
     let currentMonth;
 
-    this.getMonth = function(monthIndex) {
+    this.getMonth = getMonth;
+    this.nextMonth = nextMonth;
+    this.previousMonth = previousMonth;
+
+    function getMonth(monthIndex) {
         validateMonthIndex(monthIndex);
         currentMonth = monthIndex;
         return new MonthModel(monthIndex);
-    };
+    }
 
-    this.nextMonth = function() {
+    function nextMonth() {
         if (typeof currentMonth === 'undefined') {
             throw new Error('Can not call nextMonth when no current month set');
         }
         return (currentMonth < 11) ? this.getMonth(currentMonth + 1) : this.getMonth(0);
-    };
+    }
 
-    this.previousMonth = function() {
+    function previousMonth() {
         if (typeof currentMonth === 'undefined') {
             throw new Error('Can not call previousMonth when no current month set');
         }
         return (currentMonth > 0) ? this.getMonth(currentMonth - 1) : this.getMonth(11);
-    };
+    }
 
     function validateMonthIndex(monthIndex) {
         if (!Number.isInteger(monthIndex) || monthIndex < 0 || monthIndex > 11) {
             throw new Error('Month index must be an integer between 0 - 11');
         }
     }
+}
 
-    function MonthModel(monthIndex) {
-        this.monthName = months[monthIndex];
-    }
+function MonthModel(monthIndex) {
+    this.monthName = months[monthIndex];
 }
